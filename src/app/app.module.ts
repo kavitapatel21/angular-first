874393ee14dea2947114ca325df653b29f89e3c3
kavitapatel21 <kavita.patel@plutustec.com>
2023-02-16 18:56:08 +0530

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes,RouterModule } from '@angular/router';
+ 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+const appRoutes: Routes = [
+  {path: 'home',component: HomeComponent},
+  {path: 'about',component: AboutComponent},
+  {path: 'contact',component: ContactComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
